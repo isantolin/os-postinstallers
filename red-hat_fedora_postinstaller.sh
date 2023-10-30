9dafe9a10d5ec202782abf862452190286a88aaf
Ignacio Santolin <264640+isantolin@users.noreply.github.com>
2023-10-30 19:32:24 -0300

    added password as parameter

--- red-hat_fedora_postinstaller.sh
+++ red-hat_fedora_postinstaller.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+while getopts "p:" arg; do
+  case $arg in
+    p)
+      PASSWORD=$OPTARG  
+      ;;
+  esac
+done
+
+if [ -z "${PASSWORD}" ]; then
+	echo "Missing -p (password) parameter"
+	exit
+fi
 
 BITS=$(getconf LONG_BIT)
 ARCH=$(uname -m)
@@ -39,7 +51,7 @@ sudo find /etc/yum.repos.d/*.repo -type f -exec sed -i 's/enabled=0/enabled=1/g'
 sudo flatpak update
 sudo dnf clean all
 sudo dnf -y update --refresh
-sudo dnf -y install python-devel cairo-devel gobject-introspection-devel cairo-gobject-devel libcurl-devel krb5-devel kernel-devel-"$KERNEL" libvirt-devel pcsc-lite-devel libdb-devel akmods
+sudo dnf -y install python-devel cairo-devel gobject-introspection-devel cairo-gobject-devel libcurl-devel krb5-devel kernel-devel-"$KERNEL" libvirt-devel pcsc-lite-devel libdb-devel akmods swig
 
 # Remove conflicting packages and not used packages
 sudo dnf -y remove libswscale-free ffmpeg-free firefox
@@ -51,7 +63,7 @@ sudo mokutil --import /etc/pki/akmods/certs/public_key.der
 #### REBOOT
 
 # Install Basic Packages
-sudo dnf -y install webmin httpd gcc-c++ make nodejs cups-pdf cups-lpd cabextract lzip p7zip p7zip-plugins unrar alsa-plugins-pulseaudio libcurl gstreamer1-plugin-openh264 gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-free-fluidsynth gstreamer1-plugins-bad-free-wildmidi gstreamer1-plugins-bad-freeworld gstreamer1-plugins-base-tools gstreamer1-plugins-entrans gstreamer1-plugins-fc gstreamer1-plugins-good-extras gstreamer1-rtsp-server gstreamer1-vaapi gstreamer1-plugins-ugly NetworkManager-fortisslvpn-gnome NetworkManager-iodine-gnome NetworkManager-l2tp-gnome NetworkManager-libreswan-gnome NetworkManager-sstp-gnome NetworkManager-strongswan-gnome epson-inkjet-printer-escpr2 NetworkManager-ovs gstreamer1-libav gcc-gfortran cmake fedora-workstation-repositories perl-App-cpanminus seabios swtpm-tools code-insiders mkfontscale xset xorg-x11-drv-nvidia-390xx akmod-nvidia-390xx xorg-x11-drv-nvidia-390xx-cuda vulkan vdpauinfo libva-vdpau-driver libva-utils gstreamer1-plugin-reqwest gstreamer1-plugin-reqwest gstreamer1-plugins-bad-freeworld gstreamer1-plugins-bad-free-zbar gstreamer1-svt-av1 gstreamer1-svt-vp9 httpd mariadb-server php php-common php-mysqlnd php-xml php-json php-gd php-mbstring php-pecl-imagick php-opcache php-pecl-ssh2 php-tidy nvidia-vaapi-driver ffmpeg dnfdragora
+sudo dnf -y install webmin httpd gcc-c++ make nodejs cups-pdf cups-lpd cabextract lzip p7zip p7zip-plugins unrar alsa-plugins-pulseaudio libcurl gstreamer1-plugin-openh264 gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-free-fluidsynth gstreamer1-plugins-bad-free-wildmidi gstreamer1-plugins-bad-freeworld gstreamer1-plugins-base-tools gstreamer1-plugins-entrans gstreamer1-plugins-fc gstreamer1-plugins-good-extras gstreamer1-rtsp-server gstreamer1-vaapi gstreamer1-plugins-ugly NetworkManager-fortisslvpn-gnome NetworkManager-iodine-gnome NetworkManager-l2tp-gnome NetworkManager-libreswan-gnome NetworkManager-sstp-gnome NetworkManager-strongswan-gnome epson-inkjet-printer-escpr2 NetworkManager-ovs gstreamer1-libav gcc-gfortran cmake fedora-workstation-repositories perl-App-cpanminus seabios swtpm-tools code-insiders mkfontscale xset xorg-x11-drv-nvidia-390xx akmod-nvidia-390xx xorg-x11-drv-nvidia-390xx-cuda vulkan vdpauinfo libva-nvidia-driver gstreamer1-plugin-reqwest gstreamer1-plugin-reqwest gstreamer1-plugins-bad-freeworld gstreamer1-plugins-bad-free-zbar gstreamer1-svt-av1 gstreamer1-svt-vp9 httpd mariadb-server php php-common php-mysqlnd php-xml php-json php-gd php-mbstring php-pecl-imagick php-opcache php-pecl-ssh2 php-tidy nvidia-vaapi-driver ffmpeg dnfdragora pam-u2f pamu2fcfg pam_yubico ykpers yubikey-personalization-gui yubikey-manager yubikey-manager-qt
 
 sudo dnf -y install https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
 sudo flatpak -y install flathub io.dbeaver.DBeaverCommunity
@@ -93,7 +105,7 @@ sudo grubby --update-kernel=ALL --args='nvidia-drm.modeset=1'
 sudo grubby --update-kernel=ALL --args='video=vesafb:mtrr:3'
 sudo akmods --force
 sudo dracut --force
-sudo /usr/libexec/webmin/changepass.pl /etc/webmin root "$password"
+sudo /usr/libexec/webmin/changepass.pl /etc/webmin root "$PASSWORD"
 
 # virtio-fs folder
 mkdir ~/SHARE
